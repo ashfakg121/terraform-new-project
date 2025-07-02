560840b2d2c6869a8a38a906a0efb773bdfe33cf
localrepo2 <ashfaqgirgaonkar2@gmail.com>
2025-07-02 13:39:18 +0530

    fix: added random_id for unique S3 bucket

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,10 +1,15 @@
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "example" {
   bucket = "my-terraform-bucket-${random_id.bucket_id.hex}"
-
+  tags = {
+    Name = "TerraformBucket"
+  }
 }
 
 resource "aws_s3_bucket_acl" "example_acl" {
   bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
-
